fbc856d7565d5c1d030972e534851839c49be48a
Minh-Quan Nguyen <minhquan.nguyen.etu@univ-lille.fr>
2021-03-04 16:14:56 +0100

    Connect to mongodb from server

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,17 @@
 import app from './app';
-import {PORT} from './config/baseConfig';
+import {DATABASE_URI, PORT} from './config/baseConfig';
+import mongoose from 'mongoose';
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+mongoose.connect(DATABASE_URI!, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+}).then(() => {
+    console.log('Connected to the database');
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+
 });
+
